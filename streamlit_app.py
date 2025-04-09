ab3eb759c5a30221f59f6f595e9c6bb2a735a61a
Sven Jürgens <sven.juergens1984@googlemail.com>
2025-04-09 15:03:39 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,9 @@ st.markdown('''
     - let me know if you have found a bug or have some feature requests ;)
     
     Made by Sven Jürgens
+    
     ✨ I help apps increase their ROAS in 90 days
+    
     DM me and say hi 👇
     
     https://www.linkedin.com/in/svenjuergens/  
